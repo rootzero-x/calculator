a486bf8f4e938ab8d0744946cdcb48e7a4faf4f2
ʀᴏᴏᴛᴢᴇʀᴏ_x <rootzero.xz@gmail.com>
2026-02-19 15:11:34 +0500

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
             <div className="flex items-center justify-center min-h-screen bg-black text-white font-sans sm:bg-[#1c1c1e] md:bg-gradient-to-br md:from-gray-900 md:to-black">
                 <Routes>
                     <Route path="/" element={<Calculator />} />
-                    <Route path="/privacy" element={<PrivacyPolicy />} />
+                    <Route path="/policy" element={<PrivacyPolicy />} />
                 </Routes>
             </div>
         </BrowserRouter>
