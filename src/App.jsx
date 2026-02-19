646172759291f406cc89253bd7ca5bdef258762e
ʀᴏᴏᴛᴢᴇʀᴏ_x <rootzero.xz@gmail.com>
2026-02-19 15:09:39 +0500

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import PrivacyPolicy from './components/PrivacyPolicy';
 
 function App() {
     return (
-        <HashRouter>
+        <BrowserRouter>
             <div className="flex items-center justify-center min-h-screen bg-black text-white font-sans sm:bg-[#1c1c1e] md:bg-gradient-to-br md:from-gray-900 md:to-black">
                 <Routes>
                     <Route path="/" element={<Calculator />} />
                     <Route path="/privacy" element={<PrivacyPolicy />} />
                 </Routes>
             </div>
-        </HashRouter>
+        </BrowserRouter>
     );
 }
 
